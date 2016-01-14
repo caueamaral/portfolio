9bef2dc191e6ff2715c27fd3e3e088d9cfe34d38
Cauê Amaral <caue.amaral@hotmail.com>
2016-01-14 17:55:41 -0200

    Estrutura do menu

--- js/efeitos.js
+++ js/efeitos.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+  /* ----- logo ----- */
   setTimeout(mostrarLogo, 800);
 
 });
+
 
 
 function mostrarLogo(){
   var logo = document.querySelector('#logo');
 
   if (logo) {
-    logo.classList.add('destaque-titulo-ativo');
+    logo.classList.add('logo-ativo');
   }
 }
