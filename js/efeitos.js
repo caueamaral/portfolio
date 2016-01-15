5862d5a522604af0269481814d0090cda9eb6339
Cauê Amaral <caue.amaral@hotmail.com>
2016-01-15 17:46:59 -0200

    JavaScript dos projetos

--- js/efeitos.js
+++ js/efeitos.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', function(){
   /* ----- logo ----- */
   setTimeout(mostrarLogo, 800);
 
+
+
+  /* ----- projetos ----- */
+  window.addEventListener('scroll', mostrarProjetos);
+
 });
 
 
@@ -12,5 +17,24 @@ function mostrarLogo(){
 
   if (logo) {
     logo.classList.add('logo-ativo');
+  }
+}
+
+
+function mostrarProjetos(){
+  var rolado  = document.body.scrollTop,
+      respiro = window.innerHeight * .6;
+
+  var posicao = document.querySelector('#projetos');
+      posicao = posicao.getBoundingClientRect().top + rolado;
+
+  if (rolado > posicao - respiro) {
+    acao();
+  }
+
+  function acao() {
+    var itens = document.querySelectorAll('.projetos-item');
+
+    console.log('itens', itens);
   }
 }
