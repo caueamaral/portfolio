0158351f069e8dcea76bd31186773eda469ffdc4
Cauê Amaral <caue.amaral@hotmail.com>
2016-04-22 18:03:15 -0300

    Favicon e revisões

---
+++ elementos/topo.php
@@ -0,0 +1,5 @@
+<div class="topo">
+  <img src="img/logo-caue-amaral.png" alt="Cauê Amaral" class="topo-logo">
+  <h1 class="topo-tt">Cauê Amaral</h1>
+  <h2 class="topo-profissao">Desenvolvedor Front-end</h2>
+</div><!-- /topo -->
--- index.php
+++ index.php
@@ -5,6 +5,8 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width">
       <title>Cauê Amaral - Desenvolvedor Front-end</title>
+      <link rel="shortcut icon" href="img/favicon.ico">
+      <link rel="icon"          href="img/favicon.gif" type="image/gif">
       <link href="https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,900italic,900,700italic,400italic,700" rel="stylesheet">
       <link rel="stylesheet" type="text/css" href="css/layout.css">
       <script src="js/efeitos.js"></script>
@@ -12,20 +14,15 @@
     <body>
       <div class="container">
         <header class="fundo"></header>
-
-        <div class="topo">
-          <img src="img/logo-caue-amaral.png" alt="Cauê Amaral" class="topo-logo">
-          <h1 class="topo-tt">Cauê Amaral</h1>
-          <h2 class="topo-profissao">Desenvolvedor Front-end</h2>
-        </div>
+        <?php echo include 'elementos/topo.php'; ?>
 
         <main class="principal">
         	<?php include 'elementos/sobre.php'; ?>
         	<?php include 'elementos/habilidades.php'; ?>
 					<?php include 'elementos/portfolio.php'; ?>
-					<?php include 'elementos/redes.php'; ?>			
+					<?php include 'elementos/redes.php'; ?>
         </main><!-- /principal -->
-        
+
         <?php include 'elementos/contato.php'; ?>
       </div><!-- /container -->
     </body>
