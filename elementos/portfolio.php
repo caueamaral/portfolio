abdb9ea5bb569ca203d33047374e6e4119b0e22b
Cauê Amaral <caue.amaral@hotmail.com>
2016-04-22 18:52:32 -0300

    Portfolio

--- elementos/portfolio.php
+++ elementos/portfolio.php
@@ -2,28 +2,27 @@
 	<h3 class="portfolio-tt tt">Portfólio</h3>
 
 	<figure class="portfolio-item">
-    <img src="img/portfolio-braavo.png" alt="Braavo! E-commerce" class="portfolio-foto">
-	  <a href="#" class="portfolio-link"></a>
+		<img src="img/portfolio-braavo.png" alt="Braavo! E-commerce" class="portfolio-foto">
+		<a href="http://www.braavo.cc" class="portfolio-link"></a>
+	</figure>
+	<figure class="portfolio-item">
+		<img src="img/portfolio-birden.jpg" alt="Birden" class="portfolio-foto">
+		<a href="http://www.birden.com.br" class="portfolio-link"></a>
+	</figure>
+	<figure class="portfolio-item">
+		<img src="img/portfolio-andre.jpg" alt="André Cainelli" class="portfolio-foto">
+		<a href="http://www.andrecainelli.com.br" class="portfolio-link"></a>
 	</figure>
 	<figure class="portfolio-item">
 		<img src="img/portfolio-gapaz.jpg" alt="Gapaz" class="portfolio-foto">
-		<a href="#" class="portfolio-link"></a>
-  </figure>
+		<a href="http://www.gapaz.com.br" class="portfolio-link"></a>
+	</figure>
 	<figure class="portfolio-item">
-		<img src="img/portfolio-birden.jpg" alt="Birden" class="portfolio-foto">
-		<a href="#" class="portfolio-link"></a>
-  </figure>
-
+		<img src="img/portfolio-andre.jpg" alt="Aline Zattar" class="portfolio-foto">
+		<a href="http://www.alinezattar.com.br" class="portfolio-link"></a>
+	</figure>
 	<figure class="portfolio-item">
-		<img src="img/portfolio-gapaz.jpg" alt="Gapaz" class="portfolio-foto">
-		<a href="#" class="portfolio-link"></a>
-  </figure>
-	<figure class="portfolio-item">
-		<img src="img/portfolio-birden.jpg" alt="Birden" class="portfolio-foto">
-		<a href="#" class="portfolio-link"></a>
-  </figure>
-  <figure class="portfolio-item">
-    <img src="img/portfolio-braavo.png" alt="Braavo! E-commerce" class="portfolio-foto">
-	  <a href="#" class="portfolio-link"></a>
+		<img src="img/portfolio-andre.jpg" alt="Voa Gallery" class="portfolio-foto">
+		<a href="http://www.voagallery.com.br" class="portfolio-link"></a>
 	</figure>
 </section><!-- /portfolio secao -->
