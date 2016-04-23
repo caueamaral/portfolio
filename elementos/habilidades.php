9b1048f9a5eef271c303f63038889ba43db7c249
caue <caue.amaral@hotmail.com>
2016-04-23 15:45:49 -0300

    Revisão

--- elementos/habilidades.php
+++ elementos/habilidades.php
@@ -1,15 +1,17 @@
 <section class="habilidades secao">
 	<div class="limite">
+
 		<h3 class="habilidades-tt tt">Habilidades</h3>
 		<ul class="habilidades-lista">
-			<li class="habilidades-item habilidades-html">HTML5</li>
-			<li class="habilidades-item habilidades-css">CSS3</li>
-			<li class="habilidades-item habilidades-javascript">JavaScript</li>
-			<li class="habilidades-item habilidades-jquery">jQuery</li>
-			<li class="habilidades-item habilidades-sass">Sass</li>
-			<li class="habilidades-item habilidades-git">Git</li>
-			<li class="habilidades-item habilidades-grunt">Grunt</li>
-			<li class="habilidades-item habilidades-responsivo">Design responsivo</li>
+			<li class="habilidades-item">HTML5</li>
+			<li class="habilidades-item">CSS3</li>
+			<li class="habilidades-item">JavaScript</li>
+			<li class="habilidades-item">jQuery</li>
+			<li class="habilidades-item">Sass</li>
+			<li class="habilidades-item">Git</li>
+			<li class="habilidades-item">Grunt</li>
+			<li class="habilidades-item">Responsive Design</li>
 		</ul>
-	</div>
+
+	</div><!-- /limite -->
 </section><!-- /habilidades -->
--- elementos/sobre.php
+++ elementos/sobre.php
@@ -4,11 +4,12 @@
 		<h3 class="sobre-tt tt">Sobre mim</h3>
 		<img src="img/caue-amaral.jpg" alt="Cauê Amaral" class="sobre-foto">
 		<p class="sobre-texto">
-			Tenho 25 anos e trabalho há 5 anos como desenvolvedor front-end, atualmente na
+			Tenho 26 anos e trabalho há 6 anos como desenvolvedor front-end, atualmente na
 			<a href="http://www.braavo.cc" class="sobre-link" target="_blank">Braavo! E-commerce.</a>
 		</p>
 		<p class="sobre-texto">			
-			No meu tempo livre gosto de praticar meu inglês assistindo séries e ir ao cinema com a minha esposa.
+			No meu tempo livre gosto de praticar meu inglês assistindo séries e ir ao cinema com
+			a minha esposa adorável esposa.
 		</p>
 
 	</div><!-- /limite -->
