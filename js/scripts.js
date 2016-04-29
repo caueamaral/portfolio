ef5f380c9100188321aee95604db57785946c60f
Cauê Amaral <caue.amaral@hotmail.com>
2016-04-29 19:15:54 -0300

    PHP mailer adicionado (ainda não está funcionando)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,35 @@
 window.addEventListener('DOMContentLoaded', function(){
 
+	/* portfolio */
 	cShow.init('.portfolio-item');
 
+
+	/* contato */
+	contatoForm();
+
 });
+
+
+function contatoForm() {
+	var contatoBtn = document.querySelector('#contato-btn');
+
+	if (contatoBtn) {
+		contatoBtn.addEventListener('click', acao);
+	}
+
+	function acao(evento) {
+		evento.preventDefault();
+
+		var validado = contatoValidacao();
+
+		if (validado) {
+			var resposta = 'ok';
+
+			console.log('resposta', resposta);
+		}
+	}
+}
+
+function contatoValidacao() {
+	return true;
+}
