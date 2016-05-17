a9d0ed24a6a9ff2c74b439c868de5b679e62b01b
caue <caue.amaral@hotmail.com>
2016-05-17 18:43:36 -0300

    Título do portfólio

--- elementos/contato.php
+++ elementos/contato.php
@@ -3,7 +3,7 @@
 
 		<div class="contato-github">
 			<h2 class="contato-tt">Veja meu código</h2>
-			<a href="http://www.github.com/caueamaral" class="contato-github-link" target="_blank">Meu código no Github</a>
+			<a href="http://www.github.com/caueamaral/portfolio" class="contato-github-link" target="_blank">Meu código no Github</a>
 		</div>
 		<div class="contato-email">
 			<h2 class="contato-tt">Me mande um e-mail</h2>
