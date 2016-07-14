66e352707f7bdb295fe6e7f14553c46b1c5a4bbe
Cauê Amaral <caue.amaral@hotmail.com>
2016-07-14 00:03:34 -0300

    PORTFOLIO: revisão

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass');
+var gulp     = require('gulp'),
+    sass     = require('gulp-sass'),
+    imagemin = require('gulp-imagemin');
 
+
+/* sass */
 gulp.task('sass', function(){
-  return gulp.src('assets/sass/layout.sass')
+  gulp.src('assets/sass/*.sass')
   .pipe(sass())
   .pipe(gulp.dest('assets/css'));
 });
 
+
+/* imagemin */
+gulp.task('imagemin', function(){
+  gulp.src('assets/img/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('assets/img'));
+});
+
+
+/* watch */
 gulp.task('watch', function(){
   gulp.watch('assets/sass/*.sass', ['sass']);
 });
 
+
+/* default */
 gulp.task('default', ['watch']);
